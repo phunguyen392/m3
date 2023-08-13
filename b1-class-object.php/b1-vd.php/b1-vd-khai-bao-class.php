3dc627ce95a92a84e97ec2ed5a6d9a6a08271e61
phunguyen392 <135968425+phunguyen392@users.noreply.github.com>
2023-08-13 14:26:55 +0700

    m3

---
+++ b1-class-object.php/b1-vd.php/b1-vd-khai-bao-class.php
@@ -0,0 +1,12 @@
+<?php
+class Custormer{
+    public $name;
+    public $age;
+    public function setName($name){
+        $this->name = $name;
+    }
+    public function getAge($age){
+        return $this -> age;
+    }
+
+}
