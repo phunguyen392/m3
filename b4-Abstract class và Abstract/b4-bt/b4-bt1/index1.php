777f496816e407cb9acf3b81953f3e32ab5841cb
phunguyen392 <135968425+phunguyen392@users.noreply.github.com>
2023-08-20 23:07:45 +0700

    m3

--- b4-Abstract class và Abstract/b4-bt/b4-bt1/index1.php
+++ b4-Abstract class và Abstract/b4-bt/b4-bt1/index1.php
@@ -1,75 +1,23 @@
 <?php
+ interface Resizeable{
+  public function resize($percent);
 
-use Circle as GlobalCircle;
-
-interface Resizeable {
-   function resize($percent);
+ };
+ class Circle implements Resizeable{
+   public $radius;
+function __construct($radius)
+{
+   $this->radius = $radius;
 }
-
-class Circle implements Resizeable {
-   private $radius;
-
-   public function __construct($radius) {
-      $this->radius = $radius;
-   }
-
-   public function resize($percent) {
-      $this->radius *= ($percent / 100);
-   }
-
-   public function getArea() {
-      return pi() * $this->radius * $this->radius;
-   }
+public function getArea(){
+   return $this->radius * $this->radius * pi();
 }
+public function resize($percent){
+   $this->radius *= (1 + $percent / 100);
+   return $this->getArea();}
 
-class Rectangle implements Resizeable {
-   private $width;
-   private $height;
 
-   public function __construct($width, $height) {
-      $this->width = $width;
-      $this->height = $height;
-   }
-
-   public function resize($percent) {
-      $this->width *= ($percent / 100);
-      $this->height *= ($percent / 100);
-   }
-
-   public function getArea() {
-      return $this->width * $this->height;
-   }
 }
-
-class Square implements Resizeable {
-   private $side;
-
-   public function __construct($side) {
-      $this->side = $side;
-   }
-
-   public function resize($percent) {
-      $this->side *= ($percent / 100);
-   }
-
-   public function getArea() {
-      return $this->side * $this->side;
-   }
-}
-$circle = new Circle(5);
-$rectangle = new Rectangle(4, 6);
-$square = new Square(3);
-
-$shape1 = [
-    $circle,
-    $rectangle,
-    $square
-
-];
-
-
-   echo "S trước khi tăng kích thước: " . $shape->getArea() ;
-   $percent = rand(1, 100);
-   $shape->resize($percent);
-   echo "S sau khi tăng kích thước"  . $shape->getArea() ;
-   
+$circle = new Circle(3);
+echo "dien tich hinh tron la: " . $circle->getArea(). "<br>";
+echo $circle->resize(100);
